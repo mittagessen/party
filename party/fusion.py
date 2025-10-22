29d374c6e3a3cb4752a70bad73770d3261df290f
Benjamin Kiessling <mittagessen@l.unchti.me>
2025-10-22 23:46:53 +0200

    parenthesis

--- party/fusion.py
+++ party/fusion.py
@@ -216,7 +216,7 @@ class PartyAdapter(nn.Module):
     def forward(self, encoder_hidden_states: list[torch.Tensor]) -> torch.Tensor:
         os = []
         for idx, hidden_state in enumerate(encoder_hidden_states):
-            os.append(self.pruning[idx](hidden_state).flatten(-2).transpose(-1, -2)
+            os.append(self.pruning[idx](hidden_state).flatten(-2).transpose(-1, -2))
         return torch.cat(os, dim=1)
 
 
