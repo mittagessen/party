6d16de7c028f24b6877847272957cb6b2c24534e
Benjamin Kiessling <mittagessen@l.unchti.me>
2026-03-02 18:28:22 +0100

    Add pos embedding to keys

--- party/modules/attention.py
+++ party/modules/attention.py
@@ -290,6 +290,9 @@ class MultiHeadAttention(nn.Module):
             k = k.view(b, s_y, -1, self.head_dim)
             v = v.view(b, s_y, -1, self.head_dim)
 
+            if self.pos_embeddings is not None:
+                k = self.pos_embeddings(k, input_pos=input_pos)
+
             # k,v shape: [b, n_kv, s_y, h_d]
             k = k.transpose(1, 2)
             v = v.transpose(1, 2)
